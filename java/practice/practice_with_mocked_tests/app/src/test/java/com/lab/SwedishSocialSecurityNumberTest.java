2ac94379122c81364eda46f85fbd3320ac387626
bowi <bw222fy@student.lnu.se>
2025-11-23 18:42:54 +0100

    Before refactoring mocks

--- java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SwedishSocialSecurityNumberTest.java
+++ java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SwedishSocialSecurityNumberTest.java
@@ -3,6 +3,8 @@ package com.lab;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SwedishSocialSecurityNumberTest {
     
@@ -11,6 +13,7 @@ public class SwedishSocialSecurityNumberTest {
     @BeforeEach
     public void setUp() {
         helper = new SSNHelper();
+
     }
     
     @Test
@@ -21,5 +24,111 @@ public class SwedishSocialSecurityNumberTest {
         assertEquals("01", ssn.getMonth());
         assertEquals("01", ssn.getDay());
         assertEquals("0017", ssn.getSerialNumber());
+    }
+
+    @Test
+    public void isCorrectLength() throws Exception {
+        SSNHelper ssnMock = mock(SSNHelper.class);
+
+        when(ssnMock.isCorrectLength("900101-0017")).thenReturn(true);
+        when(ssnMock.isCorrectFormat("900101-0017")).thenReturn(true);
+        when(ssnMock.isValidDay("01")).thenReturn(true);
+        when(ssnMock.isValidMonth("01")).thenReturn(true);
+        when(ssnMock.luhnIsCorrect("900101-0017")).thenReturn(true);
+
+        Exception message = assertThrows(Exception.class, () -> new SwedishSocialSecurityNumber("90010-0017", ssnMock));
+        assertEquals("To short, must be 11 characters", message.getMessage());
+    }
+
+    @Test
+    public void isCorrectLuhn() throws Exception {
+        SSNHelper ssnMock = mock(SSNHelper.class);
+
+        when(ssnMock.isCorrectLength("9001010017")).thenReturn(true);
+        when(ssnMock.isCorrectFormat("9001010017")).thenReturn(true);
+        when(ssnMock.isValidDay("01")).thenReturn(true);
+        when(ssnMock.isValidMonth("01")).thenReturn(true);
+        when(ssnMock.luhnIsCorrect("900101-0017")).thenReturn(true);
+
+        Exception message = assertThrows(Exception.class, () -> new SwedishSocialSecurityNumber("9001010017", ssnMock));
+        assertEquals("Invalid SSN according to Luhn's algorithm", message.getMessage());
+    }
+
+    @Test
+    public void trimsCorrectly() throws Exception {
+        SSNHelper ssnMock = mock(SSNHelper.class);
+
+        when(ssnMock.isCorrectLength("900101-0017")).thenReturn(true);
+        when(ssnMock.isCorrectFormat("900101-0017")).thenReturn(true);
+        when(ssnMock.isValidDay("01")).thenReturn(true);
+        when(ssnMock.isValidMonth("01")).thenReturn(true);
+        when(ssnMock.luhnIsCorrect("900101-0017")).thenReturn(true);
+
+        assertDoesNotThrow(() -> new SwedishSocialSecurityNumber("     900101-0017", ssnMock));
+
+    }
+
+    @Test
+    public void isCorrectYear() throws Exception {
+        SSNHelper ssnMock = mock(SSNHelper.class);
+
+        when(ssnMock.isCorrectLength("900101-0017")).thenReturn(true);
+        when(ssnMock.isCorrectFormat("900101-0017")).thenReturn(true);
+        when(ssnMock.isValidDay("01")).thenReturn(true);
+        when(ssnMock.isValidMonth("01")).thenReturn(true);
+        when(ssnMock.luhnIsCorrect("900101-0017")).thenReturn(true);
+        
+        SwedishSocialSecurityNumber yearCheck = new SwedishSocialSecurityNumber("900101-0017", ssnMock);
+
+        assertEquals("90", yearCheck.getYear());
+    }
+
+    @Test
+    public void ssnIsIncorrectLength() {
+        assertFalse(helper.isCorrectLength("900101-001"));
+        assertFalse(helper.isCorrectLength("900101-0011111"));
+        assertFalse(helper.isCorrectLength("91-00"));
+    }
+
+    @Test
+    public void ssnIsIncorrectFormat() {
+        assertFalse(helper.isCorrectFormat("900100017"));
+        assertFalse(helper.isCorrectFormat("abcdef-abcd"));
+        assertFalse(helper.isCorrectFormat("90010/0017"));
+    }
+
+    @Test
+    public void ssnIsInvalidDay() {
+        assertFalse(helper.isValidDay("32"));
+        assertFalse(helper.isValidDay("-1"));
+        assertFalse(helper.isValidDay("0"));   
+    }
+
+    @Test
+    public void ssnIsValidDay() {
+        assertTrue(helper.isValidDay("1"));
+        assertTrue(helper.isValidDay("15"));
+        assertTrue(helper.isValidDay("31"));
+    }
+
+    @Test
+    public void ssnIsInvalidMonth() {
+        assertFalse(helper.isValidMonth("13"));
+        assertFalse(helper.isValidMonth("0"));
+        assertFalse(helper.isValidMonth("-1"));
+    }
+
+    @Test
+    public void ssnIsValidMonth() {
+        assertTrue(helper.isValidMonth("1"));
+        assertTrue(helper.isValidMonth("6"));
+        assertTrue(helper.isValidMonth("12"));
+    }
+
+    @Test
+    public void ssnIsIncorrectLuhn() {
+        assertFalse(helper.luhnIsCorrect("90010100017"));
+        assertFalse(helper.luhnIsCorrect("900101 0017"));
+        assertFalse(helper.luhnIsCorrect("900101+0017"));
     }
 }
